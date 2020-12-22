e42e5c7968b246236a3d921fe528357cc63ddc6a
Rafael Trigo <rafael.trigo.azevedo@gmail.com>
2020-12-22 15:30:21 -0300

    Task six added

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transation = await transactionsRepository.findOne(id);
+
+    if (!transation) {
+      throw new AppError('This transaction does not exist');
+    }
+
+    await transactionsRepository.delete(id);
   }
 }
 
